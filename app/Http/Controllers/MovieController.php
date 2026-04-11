cb410fb4a6ca6972feec2c9dee2bc54746f63cc4
NgocHai <nnhaia25059@cusc.ctu.edu.vn>
2026-04-11 16:24:54 +0700

    Fixx

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -10,19 +10,30 @@ class MovieController extends Controller
     {
         $hotMovies = [
             [
-                'title' => 'READY OR NOT',
-                'genre' => 'HORROR • THRILLER',
-                'duration' => '1H 35M',
-                'image' => 'https://picsum.photos/300/400?random=1',
-                'isIMAX' => true
+                'title' => 'Ready OR Not 2: HERE I COME',
+                'genre' => 'HORROR, THRILLER',
+                'duration' => 'Apr 10, 2026',
+                'image' => 'https://tse4.mm.bing.net/th/id/OIP.JvaNzXdS810BFrL6WBiDCQHaLG?w=800&h=1199&rs=1&pid=ImgDetMain&o=7&rm=3',
             ],
             [
-                'title' => 'SUPERMAN',
-                'genre' => 'ACTION • SCI-FI',
-                'duration' => '2H 15M',
-                'image' => 'https://picsum.photos/300/400?random=2',
-                'isIMAX' => false
+                'title' => 'MINIONS & MONTERS',
+                'genre' => 'ANIMATION',
+                'duration' => 'July 1, 2026',
+                'image' => 'https://tse2.mm.bing.net/th/id/OIP.p6bu_8NTkP3XVJANMdmy7QHaLH?rs=1&pid=ImgDetMain&o=7&rm=3',
+            ],
+            [
+                'title' => 'Spider-Man: Brand New Day',
+                'genre' => 'Adventure, Action',
+                'duration' => 'July 31, 2026',
+                'image' => 'https://image.tmdb.org/t/p/w500/9JCQtDCSpPR2ld55yNlEg1VwcQo.jpg',
+            ],
+            [
+                'title' => 'Moana',
+                'genre' => 'Adventure, Fantasy',
+                'duration' => 'July 10, 2026',
+                'image' => 'https://image.tmdb.org/t/p/w500/oA2LhsQwm7QEQP7LM70TBtuhzT6.jpg',
             ]
+            
         ];
 
         return view('homepage', compact('hotMovies'));
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\SeatController;
 use App\Http\Controllers\EmployeesController;
+use App\Http\Controllers\MovieController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,3 +12,6 @@ Route::get('/', function () {
 Route::resource('tickets', TicketController::class);
 Route::resource('seats', SeatController::class);
 Route::resource('employees', EmployeesController::class);
+
+Route::get('/', [MovieController::class, 'index']);
+
